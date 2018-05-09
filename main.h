4ae2a950454964e4f69a37b212b7524c612c9a50
Unknown <edouard.chatzopoulos@gmail.com>
2018-05-09 17:33:37 +0200

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+extern int thread_limit;
+extern bool draw_everyf;
+extern int files_number;
+extern buffer_size;
+extern struct node *buffer;
+extern struct node *head;
+extern struct node *tail;
+
+extern double best_average;
+extern struct fractal *best;
