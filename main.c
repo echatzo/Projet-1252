2f8c471dfc580ee01e70729def86876ae056c2f8
Unknown <edouard.chatzopoulos@gmail.com>
2018-05-09 17:04:45 +0200

    add main

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <semaphore.h>
+#include <stdbool.h>
 #include <unistd.h>
-#include "fractal.h"
+#include "libfractal/fractal.h"
+#include "buffer.h"
+#include "main.h"
+#include "file_reader.h"
 
-int main()
+int main(int argc, char *argv[])
 {
     /* TODO */
 
