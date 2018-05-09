dd29d95f6a7860fe93f7ed7a844335f27b03895e
edouard <edouard.chatzopoulos@gmail.com>
2018-05-09 17:52:05 +0200

    main.c

--- main.c
+++ main.c
@@ -8,8 +8,24 @@
 #include "main.h"
 #include "file_reader.h"
 
+bool draw_every_fract = false;
+
+int thread_limit = 1;
+
+int files_number = 0;
+
+int buffer_size = 0;
+struct node *buffer = NULL;
+struct node *first = NULL;
+struct node *last = NULL;
+
+double best_average = 0;
+struct fract *best = NULL;
+
 int main(int argc, char *argv[])
 {
+  const char *files[argc];
+	const char *fileOut = argv[argc-1]; //finds output file name
     /* TODO */
 
     return 0;
