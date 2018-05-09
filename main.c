8d70d316e3d52e6d58edc47edbe78a52031fa176
edouard <edouard.chatzopoulos@gmail.com>
2018-05-09 19:38:05 +0200

    main additions

--- main.c
+++ main.c
@@ -8,6 +8,9 @@
 #include "main.h"
 #include "file_reader.h"
 
+#define ARGOPT_D "-d"
+#define ARGOPT_MAXTHREADS "--maxthreads"
+
 bool draw_every_fract = false;
 
 int thread_limit = 1;
@@ -26,6 +29,21 @@ int main(int argc, char *argv[])
 {
   const char *files[argc];
 	const char *fileOut = argv[argc-1]; //finds output file name
+
+  int o = 0;
+  for (o = 1; o < argc - 1; o ++){
+    if (strcmp(argv[o], ARGOPT_D) == 0){  //searches for option -d
+      draw_every_fract = true;
+  } else if ( strcmp(argv[o], ARGOPT_MAXTHREADS) == 0) {
+      o++;
+      if (o == argc) break;
+      thread_limit = atoi(argv[o]);
+    } else {
+      files[files_number] = argv[o];
+      files_number++;
+    }
+    }
+  }
     /* TODO */
 
     return 0;
