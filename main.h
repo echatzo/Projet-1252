885ddc530935a3f132b4f6a400b1f3f4d552b019
edouard <edouard.chatzopoulos@gmail.com>
2018-05-09 17:42:07 +0200

    adding global variables to main.h

--- main.h
+++ main.h
@@ -1,13 +1,19 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+//maximum number of calculation threads
 extern int thread_limit;
-extern bool draw_everyf;
+//true if d option is in use, false otherwise
+extern bool draw_every_fract;
+//number of files
 extern int files_number;
+//size of the buffer
 extern buffer_size;
+//buffer
 extern struct node *buffer;
-extern struct node *head;
-extern struct node *tail;
-
+extern struct node *first;
+extern struct node *last;
+//best average of the fractales
 extern double best_average;
+//factal which has the best average
 extern struct fractal *best;
