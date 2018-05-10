848da58de1a794f77c7e44d1e7502e8adc0cf5a7
Clément <clement.chardon@student.uclouvain.be>
2018-05-10 17:09:24 +0200

    fract_name

--- file_reader.c
+++ file_reader.c
@@ -4,23 +4,23 @@
 
 #include "file_reader.h"
 
-void *file_reader (char *name){
+void *file_reader (char *file_name){
 
   File *to_read = NULL;
-  *to_read = fopen(name,"r");
+  *to_read = fopen(file_name,"r");
 
   if (to_read == NULL){
     perror("Error : ");
   }
 
   struct fractal *new_fract = NULL;
-  char line[500], name[64];
+  char line[500], fract_name[64];
 	int width, height;
   double a, b;
 
   while(fgets(line, 500, to_read)){
-    if (line[0] != '#' && sscanf(line, "%s %d %d %lf %lf", name, &width, &height, &a, &b) == 5){
-      new_fract = fractal_new(name, width, height, a, b);
+    if (line[0] != '#' && sscanf(line, "%s %d %d %lf %lf", fract_name, &width, &height, &a, &b) == 5){
+      new_fract = fractal_new(fract_name, width, height, a, b);
     }
     else {
       perror("Error : ");
